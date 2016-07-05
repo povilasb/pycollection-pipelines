219c31d2594ade8a600bfc788364e4e5edef77c4
Povilas Balciunas <balciunas90@gmail.com>
2016-07-05 16:29:24 +0300

    Do not crash if matplotlib is not installed

--- collection_pipelines/__init__.py
+++ collection_pipelines/__init__.py
@@ -2,4 +2,8 @@ from collection_pipelines.http import *
 from collection_pipelines.json import *
 from collection_pipelines.text import *
 from collection_pipelines.std import *
-from collection_pipelines.graph import *
+
+try:
+    from collection_pipelines.graph import *
+except:
+    pass
