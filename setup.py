7d3a724014098d31ef407269dd76dcafd233f750
Povilas Balciunas <balciunas90@gmail.com>
2016-07-07 21:53:31 +0300

    Specify requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='collection-pipelines',
-    version='0.1.0',
+    version='0.1.3',
     description='Framework to implement collection pipelines in python.',
     long_description=open('README.rst').read(),
     url='https://github.com/povilasb/pycollection-pipelines',
@@ -10,4 +10,5 @@ setup(
     author_email='balciunas90@gmail.com',
     license='MIT',
     packages=['collection_pipelines'],
+    install_requires=['typing==3.5.2.2'],
 )
