898c0515f714d86cbf2953871502144829c41be6
Povilas Balciunas <balciunas90@gmail.com>
2016-07-07 20:54:51 +0300

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='collection-pipelines',
+    version='0.1.0',
+    description='Framework to implement collection pipelines in python.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/povilasb/pycollection-pipelines',
+    author='Povilas Balciunas',
+    author_email='balciunas90@gmail.com',
+    license='MIT',
+    packages=['collection_pipelines'],
+)
